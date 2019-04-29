1736e0f7d04b96fca5659b30307d202e09810bd7
hrvoje <hrvoje.viskovic@gmail.com>
2019-04-29 19:41:14 +0200

    input samo brojevi

--- main.py
+++ main.py
@@ -35,24 +35,31 @@ def login():
 
 @app.route("/result", methods=["POST"])
 def result():
-    guess = int(request.form.get("guess"))
-    email_address = request.cookies.get("email")
+    while True:
+        try:
+            guess = int(request.form.get("guess"))
+            email_address = request.cookies.get("email")
 
-    user = User.fetch_one(query=["email", "==", email_address])
+            user = User.fetch_one(query=["email", "==", email_address])
 
-    if guess == user.secret:
-        message = ("You've guessed it {1} - congratulations! It's number {0}").format(guess, user.name)
-        User.edit(obj_id=user.id, email=None)
-        return render_template("new-game.html", message=message)
-    elif guess < 1 or guess > 30:
-        message = ("You didn't enter a number between 1 and 30")
-        return render_template("result.html", message=message)
-    elif guess > user.secret:
-        message = ("Your guess is not correct... try something smaller")
-        return render_template("result.html", message=message)
-    elif guess < user.secret:
-        message = ("Your guess is not correct... try something bigger")
-        return render_template("result.html", message=message)
+            if guess == user.secret:
+                message = ("You've guessed it {1} - congratulations! It's number {0}").format(guess, user.name)
+                User.edit(obj_id=user.id, email=None)
+                return render_template("new-game.html", message=message)
+                break
+            elif guess < 1 or guess > 30:
+                message = ("You didn't enter a number between 1 and 30")
+                return render_template("result.html", message=message)
+            elif guess > user.secret:
+                message = ("Your guess is not correct... try something smaller")
+                return render_template("result.html", message=message)
+            elif guess < user.secret:
+                message = ("Your guess is not correct... try something bigger")
+                return render_template("result.html", message=message)
+        except ValueError:
+            message = ("You didn't enter a number")
+            return render_template("result.html", message=message)
+            continue
 
 
 if __name__ == '__main__':
